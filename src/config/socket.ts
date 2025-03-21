0c998306cea2fc444a0f61fc6875e202a392cfda
Pranish <iampranish@outlook.com>
2025-03-21 13:24:07 +1100

    remove dist

--- src/config/socket.ts
+++ src/config/socket.ts
@@ -7,7 +7,7 @@ const server = http.createServer(app);
 
 const io = new Server(server, {
   cors: {
-    origin: ["http://localhost:3000"],
+    origin: ["http://192.168.1.104:3000"],
   },
 });
 
@@ -15,12 +15,16 @@ const io = new Server(server, {
 io.on("connection", (socket) => {
   console.log("A new connection has been made: ", socket.id);
 
-  socket.on("send_message", (message) => {
-    io.emit("receive_message", message);
+  socket.on("sendMessage", (message) => {
+    console.log("Received message:", message);
+
+    // Broadcast the message to all connected clients
+    io.emit("receiveMessage", message);
   });
 
+  // Handle disconnect event
   socket.on("disconnect", () => {
-    console.log("A user disconnected:", socket.id);
+    console.log("A user disconnected");
   });
 });
 
